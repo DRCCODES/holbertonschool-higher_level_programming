99e64a16a4761ab7ce6cd64abc97d1eb91c512b8
DRCCODES <1723@holbertonschool.com>
2020-04-27 21:37:57 +0000

    fixed formatting

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,8 +1,7 @@
 #!/usr/bin/pythopn3
 def magic_calculation(a, b):
-	return(98 + a**b)
+    return(98 + a**b)
 
 if __name__ == '__main__':
-	import dis
-	dis.dis(magic_calculation)
-
+    import dis
+    dis.dis(magic_calculation)
