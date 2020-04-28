5c92997015b6ad1557436217461716683fba1299
DRCCODES <1723@holbertonschool.com>
2020-04-28 18:14:27 +0000

    added 1-last_digit.py

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+ld = number
+if ld < 0:
+    ld = (ld * -1) % 10
+else:
+    ld = ld % 10
+if number < 0 and ld != 0:
+        print("Last digit of {:d} is {:d} and\
+is less than 6 and not 0".format(number, ld * -1))
+elif ld == 0:
+        print("Last digit of {:d} is {:d} and\is 0".format(number, ld))
+elif ld > 0:
+    if ld < 6:
+        print("Last digit of {:d} is {:d} and\
+is less than 6 and is not 0".format(number, ld))
+    else:
+        print("Last digit of {:d} is {:d} and\
+is greater than 5 and is not 0".format(number, ld))
