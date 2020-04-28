9f032ecf356145a4258a0a3c5237a61418db8e4b
DRCCODES <1723@holbertonschool.com>
2020-04-28 18:22:09 +0000

    fixed greater than message

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,11 +10,11 @@ if number < 0 and ld != 0:
         print("Last digit of {:d} is {:d} and\
  is less than 6 and not 0".format(number, ld * -1))
 elif ld == 0:
-        print("Last digit of {:d} is {:d} and\is 0".format(number, ld))
+        print("Last digit of {:d} is {:d} and is 0".format(number, ld))
 elif ld > 0:
     if ld < 6:
         print("Last digit of {:d} is {:d} and\
  is less than 6 and is not 0".format(number, ld))
     elif ld > 5:
         print("Last digit of {:d} is {:d} and\
- is greater than 5 and is not 0".format(number, ld))
+ is greater than 5".format(number, ld))
