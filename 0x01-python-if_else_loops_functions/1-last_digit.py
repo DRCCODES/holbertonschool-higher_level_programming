1dd401056e5af707e0a9cbfcb982cafea071a414
DRCCODES <1723@holbertonschool.com>
2020-04-28 18:17:53 +0000

    fixedspacing

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,13 +8,13 @@ else:
     ld = ld % 10
 if number < 0 and ld != 0:
         print("Last digit of {:d} is {:d} and\
-is less than 6 and not 0".format(number, ld * -1))
+ is less than 6 and not 0".format(number, ld * -1))
 elif ld == 0:
         print("Last digit of {:d} is {:d} and\is 0".format(number, ld))
 elif ld > 0:
     if ld < 6:
         print("Last digit of {:d} is {:d} and\
-is less than 6 and is not 0".format(number, ld))
-    else:
+ is less than 6 and is not 0".format(number, ld))
+    elif ld > 5:
         print("Last digit of {:d} is {:d} and\
-is greater than 5 and is not 0".format(number, ld))
+ is greater than 5 and is not 0".format(number, ld))
