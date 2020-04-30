e67b0448ad5521c2e825817ae6974ae498cc1b92
DRCCODES <1723@holbertonschool.com>
2020-04-30 17:16:08 +0000

    fixed numbering

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:
         print("0 arguments.")
     if len(sys.argv) > 1:
-        print("{:d} arguments:".format(len(sys.argv)))
+        print("{:d} arguments:".format(len(sys.argv) - 1))
         for i in range(1, len(sys.argv)):
             print("{:d}: {:s}".format(i, sys.argv[i]))
