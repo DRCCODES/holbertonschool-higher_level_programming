bb46019fd8da0b123b8e5a905bed912adce6d7a4
DRCCODES <1723@holbertonschool.com>
2020-04-30 17:35:57 +0000

    fixed formatting

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,8 +2,7 @@
 if __name__ == "__main__":
     import sys
     sum = 0
-    if len(sys.argv) > 1:    
+    if len(sys.argv) > 1:
         for num in range(1, len(sys.argv)):
             sum += int(sys.argv[num])
     print(sum)
-
