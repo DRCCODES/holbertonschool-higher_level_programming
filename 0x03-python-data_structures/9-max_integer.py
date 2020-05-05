5e96a3cb502412ff415d8119781289fc10d2c1c4
DRCCODES <1723@holbertonschool.com>
2020-05-05 18:27:54 +0000

    checks for empty

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     size = len(my_list)
+    if size == 0:
+        return None
     for i in range(size - 1):
         for j in range(0, size - i - 1):
             if my_list[j] > my_list[j + 1]:
