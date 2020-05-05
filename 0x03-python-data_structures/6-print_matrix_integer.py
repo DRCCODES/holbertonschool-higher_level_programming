b3f78f5d7f14b708f9202a6604c4a18ae24d25ab
DRCCODES <1723@holbertonschool.com>
2020-05-05 19:39:30 +0000

    using row now

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for el in matrix:
-        for e in range(0, len(el)):
-            if e < len(el) - 1:
-                print("{:d} ".format(el[e]), end="")
+    for row in matrix:
+        for e in row:
+            if e is not row[-1]:
+                print("{:d}".format(e), end=" ")
             else:
-                print("{:d}".format(el[e]))
-        if len(el) <= 1:
-            print()
+                print("{:d}".format(e), end="")
+        print()
