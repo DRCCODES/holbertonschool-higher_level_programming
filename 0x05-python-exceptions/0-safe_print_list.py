8a063ae56782f82d07e0ed115004d27bdef431c6
DRCCODES <1723@holbertonschool.com>
2020-05-18 17:38:08 +0000

    shold work for size 0 now

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    size = 0
     for num in range(0, x):
         try:
             print(my_list[num], end="")
+            size += 1
         except IndexError:
-            print()
-            return num
+            break
     print()
-    return num + 1
+    return size
