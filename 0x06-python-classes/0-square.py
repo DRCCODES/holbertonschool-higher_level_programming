01336e014e352fb4b97bef7775881037d88382b3
DRCCODES <1723@holbertonschool.com>
2020-05-19 19:52:19 +0000

    added README and TASK 0 files

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Doc For Class Square """
+
+
+
+class Square():
+
+    """ Squares got 4 sides 
+
+        Attributes: None
+
+    """
+
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+CLASSEY CLASSES!!!!
