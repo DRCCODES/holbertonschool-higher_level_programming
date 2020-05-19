234f54af8d61da2573444273f59be4c71e2e865e
DRCCODES <1723@holbertonschool.com>
2020-05-19 20:40:54 +0000

    added file for Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Doc DOc For Class Square """
+
+class Square():
+
+    def __init__(self, size):
+
+        """ init self and size
+
+        Args:
+            size is int that determines deminions
+            of the square
+
+        """
+
+        self.__size = size
