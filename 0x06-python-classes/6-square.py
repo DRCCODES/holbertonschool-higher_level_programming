e4cb03c48ecdc55444c057006abae85fbe752b54
DRCCODES <1723@holbertonschool.com>
2020-05-20 00:25:35 +0000

    added more condiiton statments

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -20,16 +20,16 @@ class Square:
         else:
             self.__size = size
 
-        if not isinstance(value, tuple):
-            raise TypeError("position must be a tuple of 2 positive integers")
+        if not isinstance(position, tuple):
+            raise TypeError("position must be a tuple of 2 positive integer ")
         elif len(position) < 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif position[0] < 0 or value[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
+            raise TypeError("position must be a tuple of 2 positive integer ")
+        elif position[0] < 0 or position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integer ")
         elif (type(position[0]) is not int or type(position[1]) is not int):
-            raise TypeError("position must be a tuple of 2 positive integers")
+            raise TypeError("position must be a tuple of 2 positive integer ")
         else:
-            self._position = position
+            self.__position = position
 
     @property
     def size(self):
