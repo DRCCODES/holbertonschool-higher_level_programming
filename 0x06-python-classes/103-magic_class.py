ac2e4c1dcd5316030e79593050d8cee7983c101b
DRCCODES <1723@holbertonschool.com>
2020-05-20 03:20:16 +0000

    correct line count now

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,35 +1,21 @@
 #!/usr/bin/python3
-
-
+""" Magic Calcultor """
 import math
 
 
-""" Magic Calculator """
-
-
 class MagicClass:
-
     """ class with radius gets Area and Circumference """
-
     def __init__(self, radius):
-
         """get radius"""
-
         self.radius = 0
-
         if type(radius) is not float and is not int:
             raise TypeError("radius must be a number")
-
         self.radius = radius
 
     def area(self):
-
         """ Get area! """
-
         return (math.pi * (self.__radius ** 2))
 
     def circumference(self):
-
         """ Get Circumference """
-
         return (2 * math.pi * self.__radius)
