bed10dfc2dfd6805fa6d8093dd6cfde13149a0be
DRCCODES <1723@holbertonschool.com>
2020-05-20 00:41:47 +0000

    fixed elif order

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -24,9 +24,9 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integer ")
         elif len(position) < 2:
             raise TypeError("position must be a tuple of 2 positive integer ")
-        elif position[0] < 0 or position[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integer ")
         elif (type(position[0]) is not int or type(position[1]) is not int):
+            raise TypeError("position must be a tuple of 2 positive integer ")
+        elif position[0] < 0 or position[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integer ")
         else:
             self.__position = position
@@ -65,9 +65,9 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         elif len(value) < 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif value[0] < 0 or value[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
         elif (type(value[0]) is not int or type(value[1]) is not int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self._position = value
