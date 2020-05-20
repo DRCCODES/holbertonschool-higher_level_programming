af53caf8238b3bee812d7564443b3dca96c3e606
DRCCODES <1723@holbertonschool.com>
2020-05-20 03:30:03 +0000

    fixed type if error of missing it

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -8,7 +8,7 @@ class MagicClass:
     def __init__(self, radius=0):
         """get radius"""
         self.__radius = 0
-        if type(radius) is not int and is not float:
+        if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
         self.__radius = radius
 
