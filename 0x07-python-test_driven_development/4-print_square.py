21fa18941aa6553980b507bb3ace58646806fd3e
DRCCODES <1723@holbertonschool.com>
2020-05-22 23:57:52 +0000

    added task 3 files

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """ prints a square """
 
+
 def print_square(size):
     sq = []
-    
-    if not isinstance(size,(int, float):
+
+    if not isinstance(size, (int)):
         raise TypeError("size must be an integer")
-    elif is isinstance(size,(float) and size < 0):
+    elif isinstance(size, (float)) and size < 0:
         raise TypeError("size must be an integer")
     elif size < 0:
-        raise TypeError("size must be >= 0")
+        raise ValueError("size must be >= 0")
 
-    for row in range(size):
+    for row in range(int(size)):
         for columns in range(size):
             sq.append("#")
         if row != size - 1:
