6a19de658ea73c13fa5f9cd1dff322d76823a446
DRCCODES <1723@holbertonschool.com>
2020-05-23 04:57:56 +0000

    added test files unrand Heeh!

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -4,7 +4,39 @@
 
 def matrix_mul(m_a, m_b):
     """ Pythonicallys Doing things without numpy """
+    if not isinstance(m_a, list):
+        raise TypeError("m_a must be a list")
+    if not isinstance(m_b, list):
+        raise TypeError("m_a must be a list")
+    for a in m_a:
+        if not isinstance(a, (list)):
+                raise TypeError(
+                    "m_a must be a list of lists")
+        if not len(m_a) or not len(a):
+            raise ValueError(
+                    "m_a can't be empty")
+        for i in a:
+            if not isinstance(i, (int, float)):
+                    raise TypeError(
+                        "m_a should contain only integers or floats")
+            if len(m_a[0]) != len(a):
+                raise ValueError(
+                    "m_a and m_b can't be multiplied")
+    for b in m_b:
+        if not isinstance(b, list):
+            raise TypeError(
+                    "m_b must be a list of list")
+        if not len(m_a) or not len(b):
+            raise ValueError("m_b can't be empty")
+        for j in b:
+            if not isinstance(j, (int, float)):
+                raise TypeError(
+                    "m_b should contain only integers or floats")
+        if len(m_b[0]) != len(b):
+            raise ValueError(
+                "m_a and m_b can't be multiplied")
 
+        
     result = [[sum(m_a * b for m_a, b in zip(m_a_row, m_b_col))
                for m_b_col in zip(*m_b)]
               for m_a_row in m_a]
--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -5,6 +5,38 @@ import numpy as np
 
 def lazy_matrix_mul(m_a, m_b):
     """ NP WINS!!! Mathtality """
+
+    if not isinstance(m_a, list):
+        raise TypeError("m_a must be a list")
+    if not isinstance(m_b, list):
+         raise TypeError("m_a must be a list")
+    for a in m_a:
+         if not isinstance(a, (list)):
+                 raise TypeError(
+                     "m_a must be a list of lists")
+         if not len(m_a) or not len(a):
+             raise ValueError(
+                     "m_a can't be empty")
+         for i in a:
+             if not isinstance(i, (int, float)):
+                     raise TypeError(
+                         "m_a should contain only integers or floats")
+             if len(m_a[0]) != len(a):
+                 raise ValueError(
+                     "m_a and m_b can't be multiplied")
+    for b in m_b:
+         if not isinstance(b, list):
+             raise TypeError(
+                     "m_b must be a list of list")
+         if not len(m_a) or not len(b):
+             raise ValueError("m_b can't be empty")
+         for j in b:
+             if not isinstance(j, (int, float)):
+                 raise TypeError(
+                     "m_b should contain only integers or floats")
+         if len(m_b[0]) != len(b):
+             raise ValueError(
+                 "m_a and m_b can't be multiplied")
     a = np.array(m_a)
     b = np.array(m_b)
     return a.dot(b)
