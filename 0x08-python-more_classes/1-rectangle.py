6b00d6c30f9d9b8654f4f36ca3fbf1d9eba305c5
DRCCODES <1723@holbertonschool.com>
2020-05-25 23:40:53 +0000

    checking capitalisation

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -24,7 +24,7 @@ class Rectangle:
 
     @property
     def width(self):
-        """ returns Width """
+        """ returns width """
         return self.__width
 
     @width.setter
@@ -33,10 +33,10 @@ class Rectangle:
 
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        
+        self.__width = value
 
     @property
     def height(self):
@@ -49,7 +49,7 @@ class Rectangle:
 
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+        
+        self.__height = value
