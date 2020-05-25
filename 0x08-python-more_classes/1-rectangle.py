e28a6f47067e5317ffbafab3ddca046ef19af1fc
DRCCODES <1723@holbertonschool.com>
2020-05-25 23:27:58 +0000

    fixed error message

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -32,9 +32,9 @@ class Rectangle:
         """ sets width to value """
 
         if not isinstance(value, int):
-            raise TypeError("value must be an integer")
+            raise TypeError("width must be an integer")
         elif value < 0:
-            raise ValueError("value must be >= 0")
+            raise ValueError("width must be >= 0")
         else:
             self.__width = value
 
@@ -48,9 +48,9 @@ class Rectangle:
         """ sets height to value """
 
         if not isinstance(value, int):
-            raise TypeError("value must be an integer")
+            raise TypeError("height must be an integer")
         elif value < 0:
-            raise ValueError("value must be >= 0")
+            raise ValueError("height must be >= 0")
         else:
             self.__height = value
 
