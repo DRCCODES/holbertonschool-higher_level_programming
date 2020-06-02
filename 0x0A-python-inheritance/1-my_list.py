f9382ee7443b4253564009a463c22c20828fd2b7
DRCCODES <1723@holbertonschool.com>
2020-06-02 21:28:33 +0000

    added files for task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#1/usr/bin/python3
+""" Doc For MyList Class which inherits from list """
+
+
+class MyList(list):
+    """ MyList in herits from list """
+
+
+    def print_sorted(self):
+        """ funtions that sorts MyList """
+
+        print(sorted(self))
