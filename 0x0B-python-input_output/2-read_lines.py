09f5c31b430a0d5f1ed363397dec68974dbacc8b
DRCCODES <1723@holbertonschool.com>
2020-06-03 22:44:29 +0000

    adding file for Task 2

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" read n num of lins and print to stdout """
+
+
+def read_lines(filename="", nb_lines=0):
+    """ reads n num lines and prints """
+    l = 0
+    with open(filename, encoding="utf-8") as f:
+        for lines in f:
+            l += 1
+        if nb_lines <= 0 or nb_lines >= l:
+            print(f.read(), end="")
+        else:
+            for n in range(nb_lines):
+                print(f.readline(), end="")
