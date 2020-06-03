f1b2f08318feb03b0cbccacf451c917ace1bf75c
DRCCODES <1723@holbertonschool.com>
2020-06-03 00:58:49 +0000

    added file for task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Square Class inherits Rectangle """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ The Class Square """
+
+    def __init__(self, size):
+        """ inits Square size and self """
+
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """ returns area of Square """
+
+        return self.__size ** 2
