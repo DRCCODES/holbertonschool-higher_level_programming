6af0ae824e483455f6dd7a4d90cc76932dc6b8fe
DRCCODES <1723@holbertonschool.com>
2020-06-04 02:45:59 +0000

    added Task 13 file

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" Doc for Class Student """
+
+
+class Student:
+    """ Student Class """
+
+    def __init__(self, first_name, last_name, age):
+        """ init for Student Class """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ returns dict of instance """
+        if not isinstance(attrs, list):
+            return self.__dict__
+
+        for i in attrs:
+            if not isinstance(i, str):
+                return self.__dict__
+        std = {}
+        for things in attrs:
+            if things in self.__dict__.keys():
+                std[things] = self.__dict__[things]
+
+        return std
+
+    def reload_from_json(self, json):
+        """ replaces all attr of studen instance """
+
+        atb = list(json.keys())
+
+        for k in atb:
+            if k in self.__dict__.keys():
+                self.__dict__[k] = json[k]
