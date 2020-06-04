c072263a046d6dd57189358d76bda2edfb8ce7d7
DRCCODES <1723@holbertonschool.com>
2020-06-04 04:02:49 +0000

    added file for task 14

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" pascal's list """
+
+
+def pascal_triangle(n):
+    """ Create Pascal's List """
+
+    pr = [1]
+    pt = []
+    row = 0
+
+    if n <= 0:
+        return []
+
+    pt.append(pr)
+
+    if n == 1:
+        return triangle
+    while row < n - 1:
+        crow = []
+        crow.append(pr[0])
+        i = 0
+        while i < row:
+            crow.append(pr[i] + pr[i + 1])
+            i += 1
+        crow.append(pr[len(pr) - 1])
+        pt.append(crow)
+        pr = crow
+        row += 1
+
+    return pt
