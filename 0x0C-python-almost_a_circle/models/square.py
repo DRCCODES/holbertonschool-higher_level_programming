6a89d0900a55ac8fff31a2cf9f7e49ee85920d6b
DRCCODES <1723@holbertonschool.com>
2020-06-08 05:11:07 +0000

    thought it would inheriate validation

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -17,4 +17,18 @@ class Square(Rectangle):
         """ over rides print for wanted format """
 
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
-                                                 self.height)
+                                                 self.width)
+
+    @property    
+    def size(self):
+        """ sets size of square """
+
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ sets sizes uses width  and height """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        self.width == value
+        self.height == value
