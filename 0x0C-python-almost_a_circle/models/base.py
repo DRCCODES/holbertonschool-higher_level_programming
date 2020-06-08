acfabf9b80c739986e60d58ba8a71f01e9325c83
DRCCODES <1723@holbertonschool.com>
2020-06-08 21:19:02 +0000

    updated base class for task 16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,10 @@
 """ Doc for BASE CLASS """
 
 
+import json
+
+
+
 class Base:
     """ Base Class """
 
@@ -15,3 +19,29 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ returns JSON of Dict """
+
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+
+        return json.dumps(list_dictionaries)
+    
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ write JSON string to rep of listobj to file """
+
+        if list_objs is None or len(list_objs) == 0:
+            listo = []
+        else:
+            listo = []
+            for item in list_objs:
+                listo.append(item.to_dictionary())
+        jstr = Base.to_json_string(listo)
+        
+        with open("{}.json".format(cls.__name__), mode="w")\
+                as f:
+            f.write(jstr)
+
