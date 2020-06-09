38470cc6c4dc1de4de255522055cdfe9923fb9af
DRCCODES <1723@holbertonschool.com>
2020-06-09 20:12:54 +0000

    added some tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,2 +1,55 @@
 #!/usr/bin/python3
 """ Doc for Unittest of class Base """
+
+
+import unittest
+from models import base
+from models.base import Base
+
+
+to_json_string = Base.to_json_string
+save_to_file = Base.save_to_file
+from_json_string = Base.from_json_string
+create = Base.create
+load_from_file = Base.load_from_file
+
+
+class TestBaseClass(unittest.TestCase):
+    """ TestBaseClass to test Base Class """
+
+    def test_doc(self):
+        """ Test for Doc """
+
+        self.assertTrue(len(base.__doc__) > 0)
+
+    def test_class_doc(self):
+        """ Test Class for Doc """
+
+        self.assertTrue(len(Base.__doc__) > 0)
+
+    def test_func_doc(self):
+        """ Test all Func for Docs """
+
+        self.assertTrue(len(to_json_string.__doc__) > 0)
+        self.assertTrue(len(save_to_file.__doc__) > 0)
+        self.assertTrue(len(from_json_string.__doc__) > 0)
+        self.assertTrue(len(create.__doc__) > 0)
+        self.assertTrue(len(load_from_file.__doc__) > 0)
+
+    """ id tests """
+
+    def test_id(self):
+        """ Test id count """
+        self.assertEqual(Base().id, 1)
+        self.assertEqual(Base().id, 2)
+        self.assertEqual(Base().id, 3)
+        self.assertEqual(Base().id, 4)
+        self.assertEqual(Base().id, 5)
+        self.assertEqual(Base().id, 6)
+        self.assertEqual(Base().id, 7)
+        bas = Base(100)
+        self.assertEqual(bas.id, 100)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,2 +1,48 @@
 #!/usr/bin/python3
 """ Doc for Unittest of class Rectangle """
+
+import sys
+import io
+import unittest
+from models.base import Base
+from models import rectangle
+from models.rectangle import Rectangle
+
+Rectangle = Rectangle
+area = Rectangle.area
+display = Rectangle.display
+update = Rectangle.update
+to_dictionary = Rectangle.to_dictionary
+
+
+class TestRectangle(unittest.TestCase):
+    """ Tests for class Rectangle """
+
+    def test_docs(self):
+        """ Tests Func Docs """
+        self.assertTrue(len(rectangle.__doc__) > 0)
+        self.assertTrue(len(Rectangle.__doc__) > 0)
+        self.assertTrue(len(area.__doc__) > 0)
+        self.assertTrue(len(display.__doc__) > 0)
+        self.assertTrue(len(update.__doc__) > 0)
+        self.assertTrue(len(to_dictionary.__doc__) > 0)
+
+    def test_getter_setter_id(self):
+        """ Test Setter, Getter and Id """
+
+        capout = io.StringIO()
+        Base._Base__nb_objects = 0
+        sys.stdout = capout
+
+        r1 = Rectangle(10, 2)
+        self.assertEqual(r1.id, 1)
+
+        r2 = Rectangle(2, 10)
+        self.assertEqual(r2.id, 2)
+
+        r3 = Rectangle(10, 2, 0, 0, 12)
+        self.assertEqual(r3.id, 12)
+
+
+if __name__ == '__main__':
+    unittest.main()
