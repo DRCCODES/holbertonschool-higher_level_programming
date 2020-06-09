d01f6fc41d6725027bc8738be6e0215db373fc31
DRCCODES <1723@holbertonschool.com>
2020-06-09 22:21:40 +0000

    added test class to square

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
 """ Doc for Unittest for class Square """
+
+
+import unittest
+import io
+import sys
+from models.base import Base
+from models.rectangle import Rectangle
+from models import square
+from models.square import Square
+Square = Square
+update = Square.update
+to_dictionary = Square.to_dictionary
+
+
+class TestSquare(unittest.TestCase):
+    """ Unittest for Class Square
