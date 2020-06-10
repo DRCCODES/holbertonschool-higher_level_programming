7ec8745655ba8296a3f9b5592256c6b087551673
DRCCODES <1723@holbertonschool.com>
2020-06-10 03:25:46 +0000

    pep8 for now!

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -4,6 +4,7 @@
 import sys
 import io
 import unittest
+import pep8
 from models.base import Base
 from models.rectangle import Rectangle
 
@@ -33,6 +34,46 @@ class TestRectangle(unittest.TestCase):
         r3 = Rectangle(10, 2, 0, 0, 12)
         self.assertEqual(r3.id, 12)
 
+        self.assertEqual(r1.width, 10)
+        self.assertEqual(r2.width, 2)
+        self.assertEqual(r3.width, 10)
+
+        self.assertEqual(r1.height, 2)
+        self.assertEqual(r2.height, 10)
+        self.assertEqual(r3.height, 2)
+
+        self.assertEqual(r1.x, 0)
+        self.assertEqual(r2.x, 0)
+        self.assertEqual(r3.x, 0)
+
+        self.assertEqual(r1.y, 0)
+        self.assertEqual(r2.y, 0)
+        self.assertEqual(r3.y, 0)
+
+        r4 = Rectangle(1, 1, 1, 1)
+        self.assertEqual(r4.x, 1)
+        self.assertEqual(r4.y, 1)
+
+    def test_area_rec(self):
+        """Test Area """
+
+        Base._Base__nb_objects = 0
+
+        r1 = Rectangle(3, 2)
+        self.assertEqual(r1.area(), 6)
+
+        r2 = Rectangle(2, 10)
+        self.assertEqual(r2.area(), 20)
+
+        r3 = Rectangle(8, 7, 0, 0, 12)
+        self.assertEqual(r3.area(), 56)
+
+    def test_pep8(self):
+        """ Test Pep Checks? """
+
+        pepboy = pep8.StyleGuide(quiet=True)
+        fallout = pepboy.check_files(["models/rectangle.py"])
+        self.assertEqual(fallout.total_errors, 0)
 
 if __name__ == '__main__':
     unittest.main()
